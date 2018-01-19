0176c546dd8db9ca96b00049e8ce30582746bc7c
wuheng <wuheng@liechengcf.com>
2018-01-19 11:13:13 +0800

    添加备案信息

--- build/usr/themes/navy/footer.php
+++ build/usr/themes/navy/footer.php
@@ -3,7 +3,7 @@
 </div>
 <footer id="footer">
     <div class="container">
-        &copy; 2012-<?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+        &copy; <?php echo date('Y'); ?> &nbsp;&nbsp; 粤ICP备18004964号 &nbsp;&nbsp;<a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1255994723'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1255994723%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
     </div>
 </footer>
